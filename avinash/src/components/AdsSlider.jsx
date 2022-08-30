9f2b111e8f172080f93d03cd012b433da778f152
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-08-30 19:49:06 +0530

    .

---
+++ avinash/src/components/AdsSlider.jsx
@@ -0,0 +1,78 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+import Slider from "react-slick";
+import { Box } from "@mui/material";
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        display: "block",
+        position: "absolute",
+        right: "100px",
+        top: "40px",
+      }}
+      onClick={onClick}
+    >
+      <ArrowForwardIosIcon
+        sx={{ position: "absolute", right: "50px", fontSize: "100px" }}
+      />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
+const settings = {
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  speed: 1000,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  //   nextArrow: <SampleNextArrow />,
+  //       prevArrow: <SamplePrevArrow />,
+  appendDots: (dots) => (
+    <div
+      style={{
+        width: "100%",
+        textAlign: "left",
+        backgroundColor: "#ffffff",
+        padding: "2px",
+      }}
+    >
+      <ul style={{ marginTop: "0px" }}> {dots} </ul>
+    </div>
+  ),
+};
+
+export const AdsSlider = () => {
+  return (
+    <Box variant="div" sx={{marginBottom:"30px"}}>
+      <Slider {...settings}>
+        <img
+          src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_1440,ar_2880:595/dpr_2/image/vm/74942fb3-e9c8-4df5-a21d-7eab65f036d5.png"
+          alt="ads"
+        />
+        <img
+          src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_1440,ar_2880:595/dpr_2/image/vm/ff9627a7-a921-4005-8021-f725a73eaeae.png"
+          alt="ads"
+        />
+      </Slider>
+    </Box>
+  );
+};
---
+++ avinash/src/components/LabTestCard.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export const LabTestCard = ({ labTest }) => {
+//   console.log(labTest)
+  return (
+    <Card
+      sx={{
+        maxWidth: 260,
+        cursor: "pointer",
+        transition: "transform .3s",
+        boxShadow:"none",
+        border:"none",
+        "&:hover": {
+            transform: "scale(0.97)",
+            boxShadow: '0 3px 6px #d3d3d3'
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="280"
+        image={labTest.image}
+        alt={labTest.title}
+        sx={{ objectFit: "fill" }}
+      />
+      <CardContent>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            fontSize: "15px",
+          }}
+        >
+          <Typography variant="div">Price</Typography>
+          <Typography
+            variant="div"
+            sx={{ fontWeight: "500", textDecoration: "line-through" }}
+          >
+            {"\u20B9"} {labTest.price}
+          </Typography>
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            fontSize: "15px",
+          }}
+        >
+          <Typography variant="div">Offer-Price</Typography>
+          <Typography
+            variant="div"
+            sx={{ fontWeight: "600", color: "rgb(71,71,71)" }}
+          >
+            {"\u20B9"} {labTest.offerPrice}
+          </Typography>
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
