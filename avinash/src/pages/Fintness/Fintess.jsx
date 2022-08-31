005b39d05213cf39ebb753dd96c2c3e4b140ac8d
Mangesh2042 <mangesh2042@gmail.com>
2022-08-31 18:40:08 +0530

    step1

---
+++ avinash/src/pages/Fintness/Fintess.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Fintess.css"
+const Fintess = () => {
+  return (
+    <>
+        <div className="container">
+            <div id='tag' className="tag">
+             
+             <div className='price_main'>
+             <div className='try'>try for free</div>
+              
+              <div className='cultpass_main'>
+                 
+                  <div className='cultpass'>
+                    <h2>cultpass</h2>
+                    <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_107,ar_3.59,q_auto:eco,dpr_1,f_auto,fl_progressive//image/test/brand-logo/cult-pass-elite-partial.png" alt="" />
+                    <h3>Starting at $1042/month</h3>
+                  </div>
+
+                  <div className='cultpass'>
+                    <h2>cultpass</h2>
+                    <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_78,ar_2.63,q_auto:eco,dpr_1,f_auto,fl_progressive//image/test/brand-logo/cult-pass-pro-partial.png" alt="" />
+                    <h3>Starting at $678/month</h3>
+                  </div>
+
+                  <div className='cultpass'>
+                    <h2>cultpass</h2>
+                    <img src=" https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_85,ar_2.86,q_auto:eco,dpr_1,f_auto,fl_progressive//image/test/brand-logo/cult-pass-live-partial-4x.png" alt="" />
+                    <h3>Starting at $117/month</h3>
+                  </div>
+              </div>
+             </div>
+              
+            </div>
+           
+            <img id='img' src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_1600,ar_1.67,q_auto:eco,dpr_1,f_auto,fl_progressive/image/vm/60446bae-890d-407b-b7b5-9896cc60b372.png" alt="" />   
+        
+        </div>
+  
+  
+    </>
+  )
+}
+
+export default Fintess
