5804a905b92fa9ed5e52f6fc9756f221de4318ce
Priyam <priyamktr7@gmail.com>
2022-09-05 17:33:08 +0530

    payment page merged

--- avinash/src/pages/Payment.jsx
+++ avinash/src/pages/Payment.jsx
@@ -5,27 +5,57 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import axios from 'axios';
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 450,
+    height:"500px",
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
   };
 
-  const Payment=()=>{
+  const Payment=({data})=>{
     const [open, setOpen] = React.useState(false);
+    const token = window.localStorage.getItem("culttoken");
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
   
+let handlepay=()=>{
+  axios({
+    method: "post",
+    url: "http://localhost:7000/checkout",
+    headers: {
+      authtoken: token,
+    },
+  })
+    .then((res) => {
+      alert(res.data.message);
+      handleClose();
+    })
+    .catch((err) => {
+    console.log(err);
+    });
+}
     return (
       <div>
-        <Button onClick={handleOpen}>Open modal</Button>
+        <Button 
+        sx={{
+          backgroundColor: "white",
+          color: "rgb(240,96,85)",
+          fontSize: "15px",
+          fontWeight: "700",
+          width: "90%",
+          "&:hover": { backgroundColor: "white", color: "black" },
+        }}
+        onClick={handleOpen}>
+          TOTAL {"\u20B9"} {data} PROCEED
+        </Button>
         <Modal
           open={open}
           onClose={handleClose}
@@ -40,8 +70,8 @@ const style = {
 
 <div className="panel panel-default credit-card-box">
 <div className="panel-heading display-table" >
-<div className="row display-tr" >
-<h3 className="panel-title display-td" >Payment Details</h3>
+<div className="row display-tr">
+<h3 className="panel-title display-td">Payment Details</h3>
 <div className="display-td" >                            
 <img className="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png"/>
 </div>
@@ -97,15 +127,15 @@ required
 </div>
 <div className="row">
 <div className="col-xs-12">
-<div className="form-group">
+{/* <div className="form-group">
 <label htmlFor="couponCode">COUPON CODE</label>
 <input type="text" className="form-control" name="couponCode" />
-</div>
+</div> */}
 </div>                        
 </div>
 <div className="row">
 <div className="col-xs-12">
-<button className="btn btn-success btn-lg btn-block" type="submit">Pay Now</button>
+<button className="btn btn-success btn-lg btn-block" type="submit" onClick={handlepay}>Pay Now</button>
 </div>
 </div>
 <div className="row" style={{display:"none"}}>
